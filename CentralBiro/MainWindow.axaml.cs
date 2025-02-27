63647ca53e8050ce6728ab53b65a87283bb9edfd
aleksandar <003aca73@gmail.com>
2025-02-27 17:50:32 +0100

    Added a method to read get http requests and to send the appropriate file

--- CentralBiro/MainWindow.axaml.cs
+++ CentralBiro/MainWindow.axaml.cs
@@ -8,6 +8,7 @@ using System.Net;
 using System.Net.Security;
 using System.Net.Sockets;
 using System.Security.Authentication;
+using System.Text;
 
 namespace CentralBiro;
 
@@ -37,6 +38,64 @@ public partial class MainWindow : Window
         catch (Exception ex) {}
     }
 
+    private string httpHeader(int statusCode, string contentType, int contentLength)
+    {
+        string header = "HTTP/1.1 " + statusCode.ToString() + "\n" +
+                        "Server: simpleSSLServer\n" +
+                        "Content-Type: " + contentType + 
+                        "\nContent-Length" + contentLength.ToString() + "\n\n";
+        
+        return header;
+    }
+
+    private string prepareHttpResponse(string httpMethod, string resourceUrl, SslStream sslStream)
+    {
+        if (httpMethod != "GET") return "";
+        if (resourceUrl == "" || resourceUrl.EndsWith("/"))
+            resourceUrl += "index.html";
+        resourceUrl = resourceUrl.Replace('/', Path.DirectorySeparatorChar);
+        string filePath = wwwPath + Path.DirectorySeparatorChar + resourceUrl;
+        if (!File.Exists(filePath)) return httpHeader(404, "Not Found", 0);
+
+        string fileData = File.ReadAllText(filePath);
+        string contentType = resourceUrl.Substring(resourceUrl.LastIndexOf('.')) switch
+        {
+            "html" => "text/html; charset=utf-8",
+            "css" => "text/css; charset=utf-8",
+            "js" => "text/javascript; charset=utf-8",
+            _ => ""
+        }; 
+        
+        return httpHeader(200, contentType, fileData.Length) + "\r\n\r\n" + fileData;
+    }
+
+    private void processRequest(SslStream stream)
+    {
+        byte[] buffer = new byte[2048];
+        StringBuilder messageData = new StringBuilder();
+        int bytes = -1;
+        do
+        {
+            bytes = stream.Read(buffer, 0, buffer.Length);
+            Decoder decoder = Encoding.UTF8.GetDecoder();
+            char[] chars = new char[decoder.GetCharCount(buffer, 0, bytes)];
+            decoder.GetChars(buffer, 0, bytes, chars, 0);
+            messageData.Append(chars);
+
+            if (messageData.ToString().IndexOf("\r\n\r\n") > -1 || messageData.ToString().IndexOf("\n\n") > -1) break;
+        } while (bytes > 0);
+        
+        int endInd = messageData.ToString().IndexOf(" HTTP/1.1");
+        messageData.Remove(endInd, messageData.ToString().Length - endInd);
+        int startInd = messageData.ToString().LastIndexOf("\n");
+        if (startInd == -1) startInd = 0;
+        messageData.Remove(0, startInd);
+        string httpMethod = messageData.ToString().Split(' ')[0];
+        string resourceUrl = messageData.ToString().Split(' ')[1];
+        
+        stream.Write(Encoding.UTF8.GetBytes(prepareHttpResponse(httpMethod, resourceUrl, stream))); 
+    }
+
     private SslStream prepareSslStream(TcpClient client)
     {
         isReadingDetailsComplete = false;
@@ -44,32 +103,35 @@ public partial class MainWindow : Window
 
         try
         {
-            sslStream.AuthenticateAsServer(sslCertificate, clientCertificateRequired: false,
-                enabledSslProtocols: SslProtocols.None, checkCertificateRevocation: false);
+            sslStream.AuthenticateAsServer(sslCertificate, false, SslProtocols.Tls12 | SslProtocols.Tls13, false);
         }
-        catch (Exception ex) {}
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
         return sslStream;
     }
 
-    private void Button_OnClick(object? sender, RoutedEventArgs e)
+    private void StartServer(object? sender, RoutedEventArgs e)
     {
         certPath = rootPathUrl + Path.DirectorySeparatorChar + "certs";
         wwwPath = rootPathUrl + Path.DirectorySeparatorChar + "www";
         getCertificate();
-        Console.Out.WriteLine(sslCertificate.GetSerialNumber());
 
         thread = new Thread(() =>
         {
-            TcpListener tcpListener = new TcpListener(IPAddress.Any, 443);
+            TcpListener tcpListener = new TcpListener(IPAddress.Any, 5000);
             tcpListener.Start();
 
             while (true)
             {
                 TcpClient client = tcpListener.AcceptTcpClient();
                 SslStream stream = prepareSslStream(client);
+                processRequest(stream);
                 stream.Close();
                 client.Close();
             }
         });
+        thread.Start();
     }
 }
