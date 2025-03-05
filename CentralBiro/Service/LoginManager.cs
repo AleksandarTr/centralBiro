0f7ba2125682d204bbed6862884c14e8f3dda6fe
aleksandar <003aca73@gmail.com>
2025-03-05 18:13:07 +0100

    Provided documentation for LoginManager.cs

--- CentralBiro/Service/LoginManager.cs
+++ CentralBiro/Service/LoginManager.cs
@@ -10,28 +10,51 @@ using Microsoft.EntityFrameworkCore;
 
 namespace CentralBiro.Service;
 
+/// <summary>
+/// <c>LoginResponse</c> struct is used to store data pertaining to the server's response to a login attempt 
+/// </summary>
+/// <param name="success">Indicates whether the login has been successful</param>
+/// <param name="token">If <see cref="Success"/> is true, contains a valid non-expired token associated with the user
+/// otherwise, it may contain any data</param>
 public struct LoginResponse(bool success, byte[] token)
 {
+    ///<value>Indicates whether the login has been successful</value>
     public bool Success { get; set; } = success;
+    ///<value>If <see cref="Success"/> is true, contains a valid non-expired token associated with the user
+    /// otherwise, it may contain any data</value>
     public byte[] Token { get; set; } = token;
 }
 
+/// <summary>
+/// <c>LoginManager</c> class is used to allow a user to login and subsequently verify that they are making requests
+/// </summary>
 public class LoginManager
 {
+    ///<value>
+    /// <c>TokenDeletionActivation</c> is a constant indication the period in milliseconds
+    /// of the <see cref="DeleteExpiredTokens"/> activation.
+    /// </value>
+    private const int TokenDeletionActivation = 5 * 60 * 1000;
+    
     private LoginManager()
     {
-        // new Thread(() =>
-        // {
-        //     while (true)
-        //     {
-        //         DeleteExpiredTokens();
-        //         Thread.Sleep(5 * 60 * 1000);
-        //     }
-        // }){IsBackground = true}.Start();
+        //Thread which periodically removes expired tokens
+        new Thread(() =>
+        {
+            while (true)
+            {
+                DeleteExpiredTokens();
+                Thread.Sleep(TokenDeletionActivation);
+            }
+        }){IsBackground = true}.Start();
     }
 
     public static LoginManager Instance { get; } = new LoginManager();
 
+    /// <summary>
+    /// <c>DeleteExpiredTokens</c> is run periodically in a thread created in the constructor
+    /// to delete any tokens that have expired.
+    /// </summary>
     private void DeleteExpiredTokens()
     {
         using var context = new CentralContext();
@@ -39,23 +62,42 @@ public class LoginManager
         context.SaveChanges();
     }
 
+    /// <summary>
+    /// <c>CalculateHashedPassword</c> is used to calculate the hashed password stored in the database
+    /// </summary>
+    /// <param name="password">User's password in plaintext</param>
+    /// <param name="salt">Salt that is stored alongside the hashed password in the user's data</param>
+    /// <returns>The hashed password represented as a byte array</returns>
     public byte[] CalculateHashedPassword(string password, byte[] salt)
     {
+        //Password is calculated with the pbkdf2 hashing algorithm using sha256
         Rfc2898DeriveBytes pbkdf2 = new Rfc2898DeriveBytes(password, salt, 5000, HashAlgorithmName.SHA256);
         byte[] result = pbkdf2.GetBytes(32);
         return result;
     }
 
+    /// <summary>
+    /// <c>AddLoggedInUser</c> is used to store a new <see cref="LoggedInUser"/> in the database.
+    /// </summary>
+    /// <param name="user">The user who has just logged in</param>
+    /// <param name="token">The token generated for the user</param>
+    /// <returns>An instance of <see cref="LoggedInUser"/> with the provided parameters</returns>
     private LoggedInUser AddLoggedInUser(User user, byte[] token)
     {
         LoggedInUser loggedInUser = new LoggedInUser(user, token);
         using var context = new CentralContext();
-        context.Attach(user);
+        context.Attach(user); //The user needs to be attached, because otherwise the context will try to add it,
+                              //resulting in a unique constraint violation
         context.LoggedInUsers.Add(loggedInUser);
         context.SaveChanges();
         return loggedInUser;
     }
     
+    /// <summary>
+    /// <c>ExtendLoggedInUser</c> extends the expiration of a user's session to the point
+    /// <see cref="LoggedInUser.LoginDuration"/> minutes from now.
+    /// </summary>
+    /// <param name="user">The user whose session is being extended</param>
     private void ExtendLoggedInUser(LoggedInUser user)
     {
         user.Expiration = DateTime.Now.AddMinutes(LoggedInUser.LoginDuration);
@@ -64,6 +106,10 @@ public class LoginManager
         context.SaveChanges();
     }
 
+    /// <summary>
+    /// <c>GenerateToken</c> is used to generate a token used to verify a user's identity with their following requests
+    /// </summary>
+    /// <returns>A newly generated token for the user in the form of a byte array</returns>
     private byte[] GenerateToken()
     {
         long timestamp = DateTime.UtcNow.Ticks;
@@ -71,6 +117,15 @@ public class LoginManager
         return sha256.ComputeHash(Encoding.UTF8.GetBytes(timestamp.ToString()));
     }
 
+    /// <summary>
+    /// <c>GetToken</c> either: <list type="bullet">
+    /// <item>fetches the token of an existing user</item>
+    /// <item>generates a token and adds the record of the user's login to the database and
+    /// returns the generated token</item>
+    /// </list>
+    /// </summary>
+    /// <param name="user">The user whose token is being fetched</param>
+    /// <returns>The user's token in the form of a byte array</returns>
     public byte[] GetToken(User user)
     {
         using var context = new CentralContext();
@@ -80,14 +135,33 @@ public class LoginManager
         return loggedInUser.Token;
     }
 
+    /// <summary>
+    /// <c>Verify</c> tests if the token provided is associated with a logged-in user whose session has not expired
+    /// </summary>
+    /// <remarks><c>Verify</c> doesn´t itself test if the token has expired, so it is possible for the token to be
+    /// marked valid, even after it has expired, but before the periodic cleanup has removed it.</remarks> 
+    /// <param name="token"></param>
+    /// <returns></returns>
     public bool Verify(byte[] token)
     {
         using var context = new CentralContext();
         LoggedInUser? loggedInUser = context.LoggedInUsers.Find(token);
-        if (loggedInUser != null) ExtendLoggedInUser(loggedInUser);
+        if (loggedInUser != null) ExtendLoggedInUser(loggedInUser);//This method is called when a user sends a request
+                                                                   //so their session gets extended
         return loggedInUser != null;
     }
 
+    /// <summary>
+    /// <c>CheckUsername</c> checks if the provided username conforms to the rules for usernames.
+    /// </summary>
+    /// <param name="username">Username to be checked</param>
+    /// <exception cref="ArgumentException">The username doesn´t conform to the rules </exception>
+    /// <remarks>The username is checked only when the user is being added into the system.<br/>
+    /// The rules, which might change, are as follows:
+    /// <list type="bullet">
+    /// <item>The username must be between 3 and 32 characters long</item>
+    /// <item>The username may only contain letters, numbers and underscores</item>
+    /// </list></remarks>
     private void CheckUsername(string username)
     {
         Regex correct = new Regex(@"^\w{3,32}$");
@@ -100,6 +174,21 @@ public class LoginManager
         throw new ArgumentException("Username can only contain letters, numbers, and underscores.");
     }
 
+    /// <summary>
+    /// <c>CheckPassword</c> checks if the provided password conforms to the rules for passwords.
+    /// </summary>
+    /// <param name="password">Password to be checked</param>
+    /// <exception cref="ArgumentException">The password doesn´t conform to the rules </exception>
+    /// <remarks>The password is checked only when the user is being added into the system.<br/>
+    /// The rules, which might change, are as follows:
+    /// <list type="bullet">
+    /// <item>The password must be between 12 and 64 characters long</item>
+    /// <item>The password may contain any character</item>
+    /// <item>The password must contain at least one lowercase letter</item>
+    /// <item>The password must contain at least one uppercase letter</item>
+    /// <item>The password must contain at least one digit</item>
+    /// <item>The password must contain at least one special character</item>
+    /// </list></remarks>
     private void CheckPassword(string password)
     {
         Regex tooShort = new Regex(@"^.{0,11}$");
@@ -116,8 +205,19 @@ public class LoginManager
         if(!hasSpecialChar.IsMatch(password)) throw new ArgumentException("Password must contain at most one special character");
     }
 
+    /// <summary>
+    /// <c>AddUser</c> adds a user to the database
+    /// </summary>
+    /// <param name="username">User's username</param>
+    /// <param name="password">User's password in plaintext</param>
+    /// <returns>Bool indicating if the user has been added successfully</returns>
+    /// <exception cref="ArgumentException">The user's username/password doesn´t
+    /// follow their respective naming rules</exception>
+    /// <exception cref="DbUpdateException">A problem caused by inserting the user record into the database,
+    /// most likely because the username is already present in the database</exception>
     public bool AddUser(string username, string password)
     {
+        //Throw exceptions in case of username/password naming violations 
         CheckUsername(username);
         CheckPassword(password);
         
@@ -131,6 +231,11 @@ public class LoginManager
         return true;
     }
 
+    /// <summary>
+    /// <c>GetUsername</c> fetches the username of a logged-in user
+    /// </summary>
+    /// <param name="token">User's non-expired token</param>
+    /// <returns>The user's username if their token is valid, otherwise null</returns>
     public string? GetUsername(byte[] token)
     {
         using var context = new CentralContext();
@@ -138,45 +243,60 @@ public class LoginManager
         return loggedInUser?.User.Username;
     }
 
+    /// <summary>
+    /// <c>GetId</c> fetches the id of a logged-in user
+    /// </summary>
+    /// <param name="token">User's non-expired token</param>
+    /// <returns>The user's id if their token is valid, otherwise null</returns>
     public int? GetId(byte[] token)
     {
         using var context = new CentralContext();
         LoggedInUser? loggedInUser = context.LoggedInUsers.Include(user => user.User).FirstOrDefault(user => user.Token == token);
         return loggedInUser?.User.Id;
     }
-
-    public User? GetUser(byte[] token)
-    {
-        using var context = new CentralContext();
-        LoggedInUser? loggedInUser = context.LoggedInUsers.Include(user => user.User).FirstOrDefault(user => user.Token == token);
-        return loggedInUser?.User;
-    }
 }
 
+/// <summary>
+/// <c>LoginController</c> class is used to receive http requests for logging in.<br/>
+/// It processes any url starting with <c>api/login</c>.
+/// </summary>
 [ApiController]
 [Route("api/login")]
 public class LoginController : ControllerBase
 {
+    /// <summary>
+    /// <c>LoginRequest</c> processes a login request and generates a token if it is valid or fetches an exisiting token
+    /// </summary>
+    /// <param name="username">User's username</param>
+    /// <param name="password">User's password in plaintext</param>
+    /// <returns><see cref="LoginResponse"/> where:<list type="bullet">
+    /// <item><see cref="LoginResponse.Success"/> is true if the request is valid and
+    /// <see cref="LoginResponse.Token"/> then contains a valid token associated with the user</item>
+    /// <item><see cref="LoginResponse.Success"/> is false if the request is invalid and
+    /// <see cref="LoginResponse.Token"/> may contain any data in that case </item>
+    /// </list></returns>
     [HttpPost]
     public IActionResult LoginRequest([FromForm] string? username = null, [FromForm] string? password = null)
     {
-        Console.WriteLine($"Login request: {username}, {password}");
+        //Check if all the arguments have been provided
         if (username == null)
             return BadRequest(new LoginResponse(false, "Username not provided"u8.ToArray()));
 
         if (password == null)
             return BadRequest(new LoginResponse(false, "Password not provided"u8.ToArray()));
 
+        //Check if the username exists in the database
         using var context = new CentralContext();
         User? user = context.Users.AsNoTracking().FirstOrDefault(u => u.Username == username);
         if (user == null)
             return NotFound(new LoginResponse(false, "No user with provided username/password combination"u8.ToArray()));
-
-
+        
+        //Check if the password matches with the user's database record
         byte[] hashedPassword = LoginManager.Instance.CalculateHashedPassword(password, user.Salt);
         if (!user.Password.SequenceEqual(hashedPassword))
             return NotFound(new LoginResponse(false, "No user with provided username/password combination"u8.ToArray()));
 
+        //Fetch a logged-in user's token/generate a new token
         byte[] token = LoginManager.Instance.GetToken(user);
         return Ok(new LoginResponse(true, token));
     }
